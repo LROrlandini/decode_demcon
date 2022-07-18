98336550ff61fc4f9eff0b34dc437cf9cacba2b9
Luciano Orlandini <luciano.r.orlandini@gmail.com>
2022-07-18 11:28:28 +0200

    Adjusted formatting

--- challenge_1/fibonacci_sum.cpp
+++ challenge_1/fibonacci_sum.cpp
@@ -4,7 +4,8 @@ int main(int argc, char * argv[])
 {
 	uint32_t first = 1, second = 2, fibonacci = 0, sum = 2;
 	
-	while (fibonacci < 4000000) {
+	while (fibonacci < 4000000)
+	{
 		fibonacci = first + second;
 		if (fibonacci % 2 == 0) sum += fibonacci;
 		first = second;
